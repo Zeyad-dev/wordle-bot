a7419e4c42fd419aa761ef6bc66d9e44e52805b9
Zeyad-dev <79684665+Zeyad-dev@users.noreply.github.com>
2022-03-27 16:27:04 +0400

    .

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -106,14 +106,14 @@ export default new Command({
         const guessed = []
         console.log(word)
         const message = '```' + `${guessed.length == 5 ? guessed.join(" ") : guessed.join(" ") + repeat('_ ', guessed.length - 5)}` + '```'
-        const msg : Message = await interaction.reply({content: `${message}`, components: components(false), fetchReply: true})
+        const msg : Message = (await interaction.reply({content: `${message}`, components: components(false), fetchReply: true}) as Message)
         const filter = (i) => {
             if(i.user.id == interaction.user.id) return true
             else return void i.reply({content: 'This game is not your\'s!', ephemeral: true})
         }
         const collector = msg.createMessageComponentCollector({
             filter,
-            type: 'BUTTON'
+            collectorType: 'BUTTON'
         })
         collector.on('collect', async (i) => {
             if(guessed.length <= 5) {
