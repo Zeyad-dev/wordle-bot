a502c0ea59966d407a4909acf49f713e478f42c9
Zeyad-dev <79684665+Zeyad-dev@users.noreply.github.com>
2022-04-06 22:09:16 +0400

    .

--- src/structures/Queue.ts
+++ src/structures/Queue.ts
@@ -111,7 +111,7 @@ export class Queue {
                             this.startGame(interaction.guild)
                             break;
                         case 'meetings':
-                            const msgg : Message = await i.reply({content: `Press one of the below buttons to set the number of emergency meetings per player.\nThe current emergency meetings per player is ${String(this.gameOptions.numberOfEmergencyMeetings)}.`, ephemeral : true, components : [
+                            const msgg = await i.reply({content: `Press one of the below buttons to set the number of emergency meetings per player.\nThe current emergency meetings per player is ${String(this.gameOptions.numberOfEmergencyMeetings)}.`, ephemeral : true, components : [
                                 new MessageActionRow()
                                 .addComponents(
                                     new MessageButton()
@@ -127,7 +127,7 @@ export class Queue {
                                     .setStyle('SECONDARY')
                                     .setCustomId('3'),
                                 )
-                            ]}) as Message
+                            ], fetchReply : true}) as Message
                             const btncollector = msgg.createMessageComponentCollector()
                             btncollector.on('collect', async (ii) => {
                                 this.gameOptions.numberOfEmergencyMeetings = parseInt(i.customId)
