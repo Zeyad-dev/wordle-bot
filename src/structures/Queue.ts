35a798b333101c7f94e824e7c5d8a8a9a773a7d3
Zeyad-dev <79684665+Zeyad-dev@users.noreply.github.com>
2022-04-06 21:44:23 +0400

    fix

--- src/structures/Queue.ts
+++ src/structures/Queue.ts
@@ -94,7 +94,7 @@ export class Queue {
                         .setLabel('Number of Imposters')
                         .setStyle('SECONDARY')
                         .setDisabled(array?.includes('imposters') || array?.includes('all') ? true : false)
-                        .setCustomId('meetings'),
+                        .setCustomId('imposters'),
                 )
         ]
         if (!editMessage) {
@@ -123,9 +123,9 @@ export class Queue {
         } else {
             const object = this.playerObject[this.playerObject.findIndex(x => x.user == interaction.user.id)]
                 if (user.id == this.host.id) {
-                    return await (object.webhook).editMessage(object.message, { content: `Players found: ${this.players.length}/**10**`, embeds: [this.embed], components: buttons()})
+                    return await object.webhook.editMessage(object.message, { content: `Players found: ${this.players.length}/**10**`, embeds: [this.embed], components: buttons()})
                 } else {
-                    return await (object.webhook).editMessage(object.message, { content: `Players found: ${this.players.length}/**10**`})
+                    return await object.webhook.editMessage(object.message, { content: `Players found: ${this.players.length}/**10**`})
                 }
             }
     }
