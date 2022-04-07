008d9460b47b14b2dda37b3c4938b0f14860e6fe
Zeyad-dev <79684665+Zeyad-dev@users.noreply.github.com>
2022-04-07 22:05:15 +0400

    .

--- src/structures/game.ts
+++ src/structures/game.ts
@@ -4,6 +4,10 @@ interface Data {
     players: User[],
     gameOptions: object
 }
+interface Data2 {
+    location: string,
+    tasks: string[]
+}
 export class Game {
     public players
     public finalPlayers : any[]
@@ -26,4 +30,88 @@ export class Game {
             this.players.splice(random, 1)
         }
     }
+    public locationsObject : Data2[] = [
+        {
+            location: 'Admin',
+            tasks: [
+                'Swipe Card'
+            ]
+        },
+        {
+            location: 'Cafeteria',
+            tasks: [
+                'Upload Data', 
+                'Empty garbage'
+            ]
+        },
+        {
+            location: 'Communications',
+            tasks: [
+                'Upload data',
+                'Divert power to communications'
+            ]
+        },
+        {
+            location: 'Electrical',
+            tasks: [
+                'Upload data', 
+                'Fix wiring', 
+                'Calibrate distributor'
+            ]
+        },
+        {
+            location: 'Engines',
+            tasks: [
+                'Align engine output',
+                'Divert power to Upper engine',
+                'Divert power to lower engine'
+            ]
+        },
+        {
+            location: 'MedBay',
+            tasks: [
+                'Submit Scan',
+                'Inspect sample'
+            ]
+        },
+        {
+            location: 'Navigation',
+            tasks: [
+                'Chart course',
+                'Upload data', 
+                'Divert power to Navigation', 
+                'Stabilize steering'
+            ]
+        },
+        {
+            location: 'O2',
+            tasks: [
+                'Empty chute', 
+                'Divert power to O2', 
+                'Clean O2 filter'
+            ]
+        },
+        {
+            location: 'Reactor',
+            tasks: [
+                'Start reactor', 
+                'Unlock manifolds'
+            ]
+        },
+        {
+            location: 'Shields',
+            tasks: [
+                'Prime Shields',
+                'Divert power to shields'
+            ]
+        },
+        {
+            location: 'Weapons',
+            tasks: [
+                'Clear Asteroids', 
+                'Upload data', 
+                'Divert Power to Weapons'
+            ]
+        },
+    ]
 }
--- src/structures/player.ts
+++ src/structures/player.ts
@@ -1,8 +1,4 @@
 import { User, Message, MessageAttachment } from 'discord.js'
-interface Data {
-    location: string,
-    tasks: string[]
-}
 export abstract class player {
     constructor(public user: User, public type: string) {}
     changeLocation(location : string, message: Message) {
@@ -14,90 +10,6 @@ export class CrewMate extends player {
     constructor(public user: User) {
         super(user, 'Crew mate')
     }
-    public locationsObject : Data[] = [
-        {
-            location: 'Admin',
-            tasks: [
-                'Swipe Card'
-            ]
-        },
-        {
-            location: 'Cafeteria',
-            tasks: [
-                'Upload Data', 
-                'Empty garbage'
-            ]
-        },
-        {
-            location: 'Communications',
-            tasks: [
-                'Upload data',
-                'Divert power to communications'
-            ]
-        },
-        {
-            location: 'Electrical',
-            tasks: [
-                'Upload data', 
-                'Fix wiring', 
-                'Calibrate distributor'
-            ]
-        },
-        {
-            location: 'Engines',
-            tasks: [
-                'Align engine output',
-                'Divert power to Upper engine',
-                'Divert power to lower engine'
-            ]
-        },
-        {
-            location: 'MedBay',
-            tasks: [
-                'Submit Scan',
-                'Inspect sample'
-            ]
-        },
-        {
-            location: 'Navigation',
-            tasks: [
-                'Chart course',
-                'Upload data', 
-                'Divert power to Navigation', 
-                'Stabilize steering'
-            ]
-        },
-        {
-            location: 'O2',
-            tasks: [
-                'Empty chute', 
-                'Divert power to O2', 
-                'Clean O2 filter'
-            ]
-        },
-        {
-            location: 'Reactor',
-            tasks: [
-                'Start reactor', 
-                'Unlock manifolds'
-            ]
-        },
-        {
-            location: 'Shields',
-            tasks: [
-                'Prime Shields',
-                'Divert power to shields'
-            ]
-        },
-        {
-            location: 'Weapons',
-            tasks: [
-                'Clear Asteroids', 
-                'Upload data', 
-                'Divert Power to Weapons'
-            ]
-        },
-    ]
 }
 export class Imposter extends player {
     constructor(public user : User) {
